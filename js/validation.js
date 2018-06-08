5c15f810877a187909f2e0f71f055be4e50d375d
GUSTAVO RIBEIRO MIRANDA <a1620304@HTO.IFSP.EDU.BR>
2018-06-08 20:16:32 -0300

    Validate email and password

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,13 @@
 function validForm(){
-    
+    let email = document.getElementById("Email").value;
+    let senha = document.getElementById("Senha").value;
+
+    if (!(senha != "" && validateEmail(email))){
+        return false;
+    }
+}
+
+function validateEmail(value){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(value).toLowerCase());
 }
