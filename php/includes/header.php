32d1b86d824ad3d85d55a185fc6145be3aecb4f4
unknown <ltiago90@gmail.com>
2018-06-17 14:38:02 -0300

    change theme color

--- php/includes/header.php
+++ php/includes/header.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0" />
-    <meta name="theme-color" content="#02adaf">
+    <meta name="theme-color" content="#1fc8db">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./fonts/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
     <link rel="stylesheet" href="./css/style.css">
